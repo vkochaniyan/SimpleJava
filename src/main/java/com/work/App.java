bd079b80724bd58b2b74d248354e5d2a8209aa85
vkochaniyan <vkochaniyan@gmail.com>
2016-07-26 15:06:56 -0400

    init version

---
+++ src/main/java/com/work/App.java
@@ -0,0 +1,16 @@
+package com.work;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String sayHello() {
+		return "Hello";
+	}
+
+	public int add(int x, int y) {
+		return x+y;
+	}
+}
